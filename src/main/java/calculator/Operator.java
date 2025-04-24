727ea6a1f43d98a5dc8dccf0c13d4bb6a5da0150
Mehran <mehran.git@hotmail.com>
2025-04-24 12:58:56 -0400

    Creer Operator class

---
+++ src/main/java/calculator/Operator.java
@@ -0,0 +1,33 @@
+package calculator;
+
+public enum Operator {
+    ADDITION('+', 1),
+    SOUSTRACTION('-', 1),
+    DIVISION('/', 2),
+    MULTIPLICATION('*', 2);
+    ;
+
+    private final char symbole;
+    private final int priorite;
+
+    Operator(char symbole, int priorite) {
+        this.symbole = symbole;
+        this.priorite = priorite;
+    }
+
+    public char getSymbole() {
+        return symbole;
+    }
+
+    public int getPriorite() {
+        return priorite;
+    }
+
+    //Retourne l'opérateur correspondant au caractère donné.
+    public static Operator fromChar(char c) {
+        for (Operator op : values()) {
+            if (op.symbole == c) return op;
+        }
+        throw new IllegalArgumentException("Opérateur inconnu: " + c);
+    }
+}
